10a66fa3e9a0e2ef4635319ae53fc6c8997cb36e
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-07-21 18:05:09 -0700

    Adds the session, which stores cookies and carries it over from a request to a response.

--- lib/phase4/controller_base.rb
+++ lib/phase4/controller_base.rb
@@ -3,14 +3,21 @@ require_relative './session'
 
 module Phase4
   class ControllerBase < Phase3::ControllerBase
+    # calls parent method and calls store session after
     def redirect_to(url)
+      super(url)
+      @session.store_session(@res)
     end
 
+    # calls parent method and calls store session after
     def render_content(content, content_type)
+      super(content, content_type)
+      @session.store_session(@res)
     end
 
-    # method exposing a `Session` object
+    # parses the request and constructs a session from it
     def session
+      @session ||= Session.new(@req)
     end
   end
 end
--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -8,29 +8,35 @@ module Phase4
     # deserialize the cookie into a hash
     def initialize(req)
       find_cookie(req)
-
     end
 
     def find_cookie(req)
       req.cookies.each do |cookie|
         if cookie.name == '_rails_lite_app'
-          cookie.value
+          @hash=JSON.parse(cookie.value)
         end
+      end
+
+      if @hash.nil?
+        @hash={}
       end
     end
 
+    # returns the requested session content
     def [](key)
+      @hash[key]
     end
 
+    # modifies the session content
     def []=(key, val)
+      @hash[key] = val
     end
 
-    # serialize the hash into json and save in a cookie
-    # add to the responses cookies
+    # serializes the hash into json
+    # saves in a cookie named '_rails_lite_app'
+    # adds to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new('_rails_lite_app', @hash.to_json)
     end
   end
 end
-
-
-# If this cookie has been set before, it should use JSON to deserialize the value and store this in an ivar; else it should store {}
